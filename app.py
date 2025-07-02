9eea2a23e485e492834aa6e14e9ab87bdcbf0ba8
Ramalakshmi Somanchi <rsomanchi@tns.org>
2025-07-02 14:39:06 +0530

    app.py

--- app.py
+++ app.py
@@ -27,50 +27,48 @@ with tab_geographic:
     st.header("Geographic Overview")
     st.write("Explore the regional distribution and characteristics through these interactive maps.")
 
-    # Datawrapper Embed 1 (Original)
+    # Datawrapper Embed 1 (Original) - min-height:598px
     st.subheader("Map 1: Overview Distribution (Oq2xV)")
     datawrapper_embed_code_1 = """
     <div style="min-height:598px" id="datawrapper-vis-Oq2xV"><script type="text/javascript" defer src="https://datawrapper.dwcdn.net/Oq2xV/embed.js" charset="utf-8" data-target="#datawrapper-vis-Oq2xV"></script><noscript><img src="https://datawrapper.dwcdn.net/Oq2xV/full.png" alt="" /></noscript></div>
     """
-    components.html(datawrapper_embed_code_1, height=600, scrolling=True)
+    components.html(datawrapper_embed_code_1, height=620, scrolling=False) # Increased height, set scrolling to False
 
     st.markdown("---") # Visual separator
 
-    # Datawrapper Embed 2 (Original)
+    # Datawrapper Embed 2 (Original) - min-height:384px
     st.subheader("Map 2: Additional Regional View (dNfZU)")
     datawrapper_embed_code_2 = """
     <div style="min-height:384px" id="datawrapper-vis-dNfZU"><script type="text/javascript" defer src="https://datawrapper.dwcdn.net/dNfZU/embed.js" charset="utf-8" data-target="#datawrapper-vis-dNfZU"></script><noscript><img src="https://datawrapper.dwcdn.net/dNfZU/full.png" alt="" /></noscript></div>
     """
-    components.html(datawrapper_embed_code_2, height=400, scrolling=True)
+    components.html(datawrapper_embed_code_2, height=410, scrolling=False) # Increased height, set scrolling to False
 
     st.markdown("---") # Visual separator
 
-    # Datawrapper Embed 3 (Original)
+    # Datawrapper Embed 3 (Original) - min-height:677px
     st.subheader("Map 3: Further Geographic Insight (7IEJR)")
     datawrapper_embed_code_3 = """
     <div style="min-height:677px" id="datawrapper-vis-7IEJR"><script type="text/javascript" defer src="https://datawrapper.dwcdn.net/7IEJR/embed.js" charset="utf-8" data-target="#datawrapper-vis-7IEJR"></script><noscript><img src="https://datawrapper.dwcdn.net/7IEJR/full.png" alt="" /></noscript></div>
     """
-    components.html(datawrapper_embed_code_3, height=700, scrolling=True)
+    components.html(datawrapper_embed_code_3, height=700, scrolling=False) # Increased height, set scrolling to False
 
     st.markdown("---") # Visual separator
 
-    # --- New Datawrapper Embed 4 (fNesy) ---
+    # New Datawrapper Embed 4 (fNesy) - min-height:114px
     st.subheader("Map 4: Quick Geographic Detail (fNesy)")
     datawrapper_embed_code_4 = """
     <div style="min-height:114px" id="datawrapper-vis-fNesy"><script type="text/javascript" defer src="https://datawrapper.dwcdn.net/fNesy/embed.js" charset="utf-8" data-target="#datawrapper-vis-fNesy"></script><noscript><img src="https://datawrapper.dwcdn.net/fNesy/full.png" alt="" /></noscript></div>
     """
-    # This map is quite short, a height of 150px should be sufficient, allowing for some padding.
-    components.html(datawrapper_embed_code_4, height=150, scrolling=True)
+    components.html(datawrapper_embed_code_4, height=140, scrolling=False) # Increased height, set scrolling to False
 
     st.markdown("---") # Visual separator
 
-    # --- New Datawrapper Embed 5 (3Fewz) ---
+    # New Datawrapper Embed 5 (3Fewz) - min-height:558px
     st.subheader("Map 5: Detailed Geographic View (3Fewz)")
     datawrapper_embed_code_5 = """
     <div style="min-height:558px" id="datawrapper-vis-3Fewz"><script type="text/javascript" defer src="https://datawrapper.dwcdn.net/3Fewz/embed.js" charset="utf-8" data-target="#datawrapper-vis-3Fewz"></script><noscript><img src="https://datawrapper.dwcdn.net/3Fewz/full.png" alt="" /></noscript></div>
     """
-    # A height of 580px seems appropriate for this one.
-    components.html(datawrapper_embed_code_5, height=580, scrolling=True)
+    components.html(datawrapper_embed_code_5, height=590, scrolling=False) # Increased height, set scrolling to False
 
 
 # --- Content for 'Demographic Data' Tab ---
